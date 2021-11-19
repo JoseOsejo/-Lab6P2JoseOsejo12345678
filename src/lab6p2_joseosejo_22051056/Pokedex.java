bdb13c694d4909a1c8085ebe77a57413d8c8dad8
JoseOsejo <80367652+JoseOsejo@users.noreply.github.com>
2021-11-19 14:46:52 -0600

    commit con las clases

--- src/lab6p2_joseosejo_22051056/Pokedex.java
+++ src/lab6p2_joseosejo_22051056/Pokedex.java
@@ -9,14 +9,6 @@ package lab6p2_joseosejo_22051056;
  *
  * @author jcoq2
  */
-public class Lab6P2_JoseOsejo_22051056 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int holaMundo = 3;
-        System.out.println("Hola Mundo"+ holaMundo);
-    }
+public class Pokedex {
     
 }
